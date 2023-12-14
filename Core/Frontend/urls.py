3f8966ea79af653384c8cfd1bdad708c2e07e3cc
arobu <arobu@student.42heilbronn.de>
2023-12-14 18:43:19 +0100

    added nav bar and learnt how to  swap templates

--- Core/Frontend/urls.py
+++ Core/Frontend/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.home, name='index'),
+    path('text', views.text, name='text'),
 ]
--- Core/Frontend/views.py
+++ Core/Frontend/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.shortcuts import redirect
 # Create your views here.
 
 
-def index(response):
-    return render(response, "home.html", {})
+def text(response):
+    return render(response, "text.html", {})
 
+
+def home(response):
+    return render(response, "home.html", {})
