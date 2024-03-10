a3050d5b6d7994aecd3f409ad5d9b86b16b43705
arobu <arobu@student.42heilbronn.de>
2024-03-10 18:33:00 +0100

    this fucking chat css

--- transcendence/chat/views.py
+++ transcendence/chat/views.py
@@ -10,6 +10,11 @@ from django.views.generic import TemplateView
 class ChatView(LoginRequiredMixin, TemplateView):
     template_name = 'chat/chat.html'
 
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, status=200, context={'messagess': [
+            'Hello bro',
+            'How are you ?'
+        ]})
+
     def post(self, request, *args, **kwargs):
-        print(self.request.body)
-        return render(request, self.template_name, status=204)
+        return render(request, self.template_name, status=200, context={'messagess': 'wtf'})
